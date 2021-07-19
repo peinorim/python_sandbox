681779377db17a8f3a3525e42656f5357b03b4f1
ET02372@zgie.ztech <arnaud.filliol@ca-gip.fr>
2021-07-19 16:37:53 +0200

    test heat

--- stats/pepper.py
+++ stats/pepper.py
@@ -2,36 +2,54 @@ import csv
 from datetime import datetime
 
 
-class Marble:
-    def __init__(self, num=None, nb_sort=None, occurence=None, last_sorted_date=None):
-        self.num = int(num)
-        self.nb_sort = int(nb_sort)
-        self.occurence = float(occurence.replace(',', '.'))
-        self.sorted_date = datetime.strptime(last_sorted_date, '%d/%m/%Y')
-
-
 class Pepper:
-    data = list()
+    heat = 0
 
-    def __init__(self, sort_by_occurence=True):
-        self.__get_data(sort_by_occurence=sort_by_occurence)
+    def __init__(self, num=None, nb_picked=None, occurence=None, last_sorted_date=None):
+        self.num = int(num)
+        self.nb_picked = int(nb_picked)
+        self.occurence = float(occurence.replace(',', '.'))
+        self.last_sorted_date = datetime.strptime(last_sorted_date, '%d/%m/%Y')
 
-    def __get_data(self, sort_by_occurence=True):
+
+class PepperPot:
+    peppers = []
+
+    def __init__(self):
+        self.__get_data()
+
+    def __get_data(self):
         with open('stats.csv', newline='') as csvfile:
             datareader = csv.reader(csvfile, delimiter=';', quotechar='|')
             for row in datareader:
-                self.data.append(
-                    Marble(num=row[0], nb_sort=row[1], occurence=row[2], last_sorted_date=row[3])
+                self.peppers.append(
+                    Pepper(num=row[0], nb_picked=row[1], occurence=row[2], last_sorted_date=row[3])
                 )
             csvfile.close()
+        self.__generate_heat()
 
-            if sort_by_occurence:
-                self.__sort_by_occurence()
+    def __generate_heat(self):
+        if self.peppers:
+            now = datetime.now()
+            for pepper in self.peppers:
+                last_picked = (now - pepper.last_sorted_date).days
+                pepper.heat = round(pepper.occurence * last_picked, 2)
 
-    def __sort_by_occurence(self):
-        self.data.sort(key=lambda x: x.occurence, reverse=True)
+            self.__sort_by_heat()
+
+    def open(self):
+        now = datetime.now()
+        print(f"Let's open our pepper pot on {now:%Y-%m-%d}.")
+        for pepper in self.peppers:
+            print(
+                f"{pepper.num} heat is {pepper.heat} ({(now - pepper.last_sorted_date).days} days not seen, {pepper.occurence}% occured)."
+            )
+        print("End of pepper pot opening")
+
+    def __sort_by_heat(self):
+        self.peppers.sort(key=lambda x: x.heat, reverse=False)
 
 
 if __name__ == '__main__':
-    pepper = Pepper()
-    print("")
+    pot = PepperPot()
+    pot.open()
