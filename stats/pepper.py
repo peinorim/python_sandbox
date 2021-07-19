e4a74127e4e973f77d71299d26d0a71fee2a8b9f
ET02372@zgie.ztech <arnaud.filliol@ca-gip.fr>
2021-07-19 10:40:39 +0200

    init

--- stats/pepper.py
+++ stats/pepper.py
@@ -1,1 +1,28 @@
+import csv
+from datetime import datetime
 
+
+class Marble:
+    def __init__(self, num=None, nb_sort=None, occurence=None, last_sorted_date=None):
+        self.num = int(num)
+        self.num = int(nb_sort)
+        self.occurence = float(occurence.replace(',', '.'))
+        self.sorted_date = datetime.strptime(last_sorted_date, '%d/%m/%Y')
+
+
+class Pepper:
+    data = list()
+
+    def __init__(self):
+        self.get_data()
+
+    def get_data(self):
+        with open('stats.csv', newline='') as csvfile:
+            datareader = csv.reader(csvfile, delimiter=';', quotechar='|')
+            for row in datareader:
+                self.data.append(Marble(num=row[0], nb_sort=row[1], occurence=row[2], last_sorted_date=row[3]))
+
+
+if __name__ == '__main__':
+    pepper = Pepper()
+    print("")
