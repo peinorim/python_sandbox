e867f6606badecbf06598d712edffb8178b22591
ET02372@zgie.ztech <arnaud.filliol@ca-gip.fr>
2021-07-19 11:44:13 +0200

    test sorting

--- stats/pepper.py
+++ stats/pepper.py
@@ -13,22 +13,25 @@ class Marble:
 class Pepper:
     data = list()
 
-    def __init__(self):
-        self.get_data()
+    def __init__(self, sort_by_occurence=True):
+        self.__get_data(sort_by_occurence=sort_by_occurence)
 
-    def get_data(self):
+    def __get_data(self, sort_by_occurence=True):
         with open('stats.csv', newline='') as csvfile:
             datareader = csv.reader(csvfile, delimiter=';', quotechar='|')
             for row in datareader:
                 self.data.append(
                     Marble(num=row[0], nb_sort=row[1], occurence=row[2], last_sorted_date=row[3])
                 )
+            csvfile.close()
 
-    def sort_by_occurence(self):
+            if sort_by_occurence:
+                self.__sort_by_occurence()
+
+    def __sort_by_occurence(self):
         self.data.sort(key=lambda x: x.occurence, reverse=True)
 
 
 if __name__ == '__main__':
     pepper = Pepper()
-    pepper.sort_by_occurence()
     print("")
