049cdb8f2f088ca29e6d23e8d4d621b81ab1d95a
ET02372@zgie.ztech <arnaud.filliol@ca-gip.fr>
2021-07-19 11:05:03 +0200

    test sorting

--- stats/pepper.py
+++ stats/pepper.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Marble:
     def __init__(self, num=None, nb_sort=None, occurence=None, last_sorted_date=None):
         self.num = int(num)
-        self.num = int(nb_sort)
+        self.nb_sort = int(nb_sort)
         self.occurence = float(occurence.replace(',', '.'))
         self.sorted_date = datetime.strptime(last_sorted_date, '%d/%m/%Y')
 
@@ -20,9 +20,15 @@ class Pepper:
         with open('stats.csv', newline='') as csvfile:
             datareader = csv.reader(csvfile, delimiter=';', quotechar='|')
             for row in datareader:
-                self.data.append(Marble(num=row[0], nb_sort=row[1], occurence=row[2], last_sorted_date=row[3]))
+                self.data.append(
+                    Marble(num=row[0], nb_sort=row[1], occurence=row[2], last_sorted_date=row[3])
+                )
+
+    def sort_by_occurence(self):
+        self.data.sort(key=lambda x: x.occurence, reverse=True)
 
 
 if __name__ == '__main__':
     pepper = Pepper()
+    pepper.sort_by_occurence()
     print("")
