bee364a7aff106d612f96a44f7ea9f861b08cc70
peinorim <peinorim@gmail.com>
2022-10-18 22:07:55 +0200

    updates

--- stats/history.py
+++ stats/history.py
@@ -284,7 +284,7 @@ class History:
 
 
 if __name__ == '__main__':
-    MAX_DATE = "2022-10-17"
+    MAX_DATE = "2022-10-18"
     EU = True
     max_date = datetime.strptime(MAX_DATE, '%Y-%m-%d')
     history = History(eu=EU, max_date=max_date)
