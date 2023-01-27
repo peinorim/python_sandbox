8691e81b13934338442762671ce10f017173dc20
peinorim <peinorim@gmail.com>
2023-01-27 14:48:45 +0100

    openai img

--- ai/image.py
+++ ai/image.py
@@ -1,16 +1,32 @@
+import os
 import webbrowser
 
 import openai
+import requests
 
 from ai import OPENAI_API_KEY
 
 openai.proxy = 'http://127.0.0.1:3128/'
 openai.api_key = OPENAI_API_KEY
 
-response = openai.Image.create(
-    prompt="a dark alley in whitechapel, London in 1888",
-    n=1,
-    size="1024x1024"
-)
+img_request = "diagon alley in the late 1800's"
+try:
 
-webbrowser.open(response['data'][0]['url'], new=0, autoraise=True)
+    ai_response = openai.Image.create(
+        prompt=img_request,
+        n=1,
+        size="1024x1024"
+    )
+
+    url = ai_response['data'][0]['url']
+
+    response = requests.get(url, proxies={"https": openai.proxy} if openai.proxy else None)
+    if response.content:
+        f = open(f"{os.getcwd()}\\images\\{img_request.replace(' ', '-').replace('.', '_')}.png", 'wb')
+        f.write(response.content)
+        f.close()
+
+except Exception as err:
+    print(err)
+
+webbrowser.open(url, new=0, autoraise=True)
