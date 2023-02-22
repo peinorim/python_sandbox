ef2aa8596c6a2fe7a4393b168cc7ef8b656c09c3
peinorim <peinorim@gmail.com>
2023-02-22 16:01:06 +0100

    fixes

--- stats/history.py
+++ stats/history.py
@@ -18,7 +18,7 @@ cmdstanpy_logger = logging.getLogger("cmdstanpy")
 cmdstanpy_logger.disabled = True
 
 PERIODS = 5
-MAX_DATE = "2023-02-20"
+MAX_DATE = "2023-02-22"
 EU = True
 GRAPHS = False
 
@@ -39,7 +39,7 @@ class Forecast:
         future = m.make_future_dataframe(periods=PERIODS)
         future_values = list(m.predict(future).yhat.values)
         low_val = future_values[-PERIODS-1]
-        high_val = future_values[-2]
+        high_val = future_values[-3]
         return [
             low_val,
             high_val,
