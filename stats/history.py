cca60c8b325743e1c0cc69357a3a0843ae73ef41
peinorim <peinorim@gmail.com>
2024-03-23 22:11:39 +0100

    updates

--- stats/history.py
+++ stats/history.py
@@ -20,8 +20,8 @@ cmdstanpy_logger = logging.getLogger("cmdstanpy")
 cmdstanpy_logger.disabled = True
 
 PERIODS = 5
-MAX_DATE = "2024-03-13"
-EU = True
+MAX_DATE = "2024-03-23"
+EU = False
 GRAPHS = False
 
 
