164803092456fbf69cab6c684b8503309f7df784
peinorim <peinorim@gmail.com>
2024-06-10 21:37:24 +0200

    dash perf

--- stock/perf.py
+++ stock/perf.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+import dash
+from dash import dcc, html
+import dash_bootstrap_components as dbc
 import yfinance as yf
-import matplotlib.pyplot as plt
+import plotly.graph_objs as go
 
 
 def download_data(tickers, start, end):
@@ -12,62 +15,70 @@ def download_data(tickers, start, end):
 def calculate_portfolio_performance(stock1, stock2, gold, start, end):
     # Télécharger les données
     data = download_data([stock1, stock2, gold], start, end)
-
     # Calculer les rendements journaliers
     returns = data.pct_change().dropna()
-
     # Définir les poids du portefeuille
-    weights = [1/3, 1/3, 1/3]  # 2/3 d'actions et 1/3 d'or
-
+    weights = [0.5, 0.25, 0.25]  # 2/3 d'actions et 1/3 d'or
     # Calculer les rendements du portefeuille
     portfolio_returns = returns.dot(weights)
-
-    # Calculer la valeur cumulée du portefeuille
-    portfolio_value = 100 * ((1 + portfolio_returns).cumprod())
-
-    return portfolio_value
+    # Calculer la valeur cumulée du portefeuille en démarrant à 100
+    return 100 * (1 + portfolio_returns).cumprod()
 
 
 def calculate_sp500_performance(start, end):
     # Télécharger les données du S&P 500
     sp500_data = download_data('^GSPC', start, end)
-
     # Calculer les rendements journaliers
     sp500_returns = sp500_data.pct_change().dropna()
-
-    # Calculer la valeur cumulée du S&P 500
-    sp500_value = 100 * ((1 + sp500_returns).cumprod())
-
-    return sp500_value
+    # Calculer la valeur cumulée du S&P 500 en démarrant à 100
+    return 100 * (1 + sp500_returns).cumprod()
 
 
-def plot_performance(portfolio_value, sp500_value):
-    plt.figure(figsize=(10, 6))
-    plt.plot(portfolio_value, label='Portfolio')
-    plt.plot(sp500_value, label='S&P 500', linestyle='--')
-    plt.title('Portfolio Performance vs S&P 500')
-    plt.xlabel('Date')
-    plt.ylabel('Cumulative Returns')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# Définir les tickers pour les actions et l'or
+stock1 = 'CSPX.L'
+stock2 = 'NDIA.L'
+gold = 'GLDD.L'
 
+# Définir la période d'analyse
+start_date = '2024-01-01'
+end_date = datetime.today().strftime('%Y-%m-%d')
 
-if __name__ == "__main__":
-    # Définir les tickers pour les actions et l'or
-    stock1 = 'CSPX.L'  # Exemple d'action 1
-    stock2 = 'NDIA.L'  # Exemple d'action 2
-    gold = 'GLDD.L'  # Ticker pour l'or (futures or)
+portfolio_value = calculate_portfolio_performance(stock1, stock2, gold, start_date, end_date)
+sp500_value = calculate_sp500_performance(start_date, end_date)
 
-    # Définir la période d'analyse
-    start_date = '2024-01-01'
-    end_date = datetime.today().strftime('%Y-%m-%d')
+app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div(className="col-md-2"),
+        html.Div([dcc.Graph(
+            id='performance-graph',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=portfolio_value.index,
+                        y=portfolio_value,
+                        mode='lines',
+                        name='Portfolio'
+                    ),
+                    go.Scatter(
+                        x=sp500_value.index,
+                        y=sp500_value,
+                        mode='lines',
+                        name='S&P 500'
+                    )
+                ],
+                'layout': go.Layout(
+                    title='Portfolio Performance vs S&P 500',
+                    xaxis={'title': 'Date'},
+                    yaxis={'title': 'Cumulative Returns'},
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+            }
+        )], className="col-md-8"),
+        html.Div(className="col-md-2"),
+    ])
+], fluid=True)
 
-    # Calculer la performance du portefeuille
-    portfolio_value = calculate_portfolio_performance(stock1, stock2, gold, start_date, end_date)
-
-    # Calculer la performance du S&P 500
-    sp500_value = calculate_sp500_performance(start_date, end_date)
-
-    # Afficher la performance du portefeuille et du S&P 500
-    plot_performance(portfolio_value, sp500_value)
+if __name__ == '__main__':
+    app.run_server(debug=True)
