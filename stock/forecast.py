46503ff16e35df6f1ba1bafd675f23d766b138f1
peinorim <peinorim@gmail.com>
2025-07-11 14:06:39 +0200

    updates

--- stock/forecast.py
+++ stock/forecast.py
@@ -1,53 +1,54 @@
-from math import inf
-
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from darts import TimeSeries
+from darts.models import Theta
+from darts.utils.timeseries_generation import datetime_attribute_timeseries
+from darts.utils.missing_values import fill_missing_values
 import plotly.graph_objects as go
 
 
 class Forecast:
 
     def render_figure(self, symbol: str = None, data=None, periods=None):
-        m = Prophet()
-        m.fit(data)
-        future = m.make_future_dataframe(periods=periods)
-        forecast = m.predict(future)
+        # m = Prophet()
+        # m.fit(data)
+        # future = m.make_future_dataframe(periods=periods)
+        # forecast = m.predict(future)
+        #
+        # forecast_fig = plot_plotly(m, forecast, uncertainty=True, plot_cap=True, trend=False, changepoints=True)
 
-        forecast_fig = plot_plotly(m, forecast, uncertainty=True, plot_cap=True, trend=False, changepoints=True)
+        # Initialisation et entraînement d’un modèle de base
+        model = Theta()  # ou ExponentialSmoothing() / LinearRegressionModel()
+        model.fit(data)
 
-        forecast_fig['layout']['showlegend'] = True
-        forecast_fig['layout']['width'] = inf
-        forecast_fig['layout']['title'] = symbol
+        # Génération de la prévision (comme `make_future_dataframe(periods=...)`)
+        forecast = model.predict(n=periods)
 
-        forecast_fig.update_layout(
-            xaxis=go.layout.XAxis(
-                tickformat="%d/%m/%Y",
-                rangeselector=dict(
-                    buttons=list([
-                        dict(count=1,
-                             label="1m",
-                             step="month",
-                             stepmode="backward"),
-                        dict(count=6,
-                             label="6m",
-                             step="month",
-                             stepmode="backward"),
-                        dict(count=1,
-                             label="YTD",
-                             step="year",
-                             stepmode="todate"),
-                        dict(count=1,
-                             label="1y",
-                             step="year",
-                             stepmode="backward"),
-                        dict(step="all", label="tout")
-                    ])
-                ),
-                rangeslider=dict(
-                    visible=True
-                ),
-                type="date"
-            ),
-            yaxis=dict(showgrid=True),
+        df_series = data.pd_dataframe()
+        df_forecast = forecast.pd_dataframe()
+
+        # ➤ Construction de la figure Plotly
+        fig = go.Figure()
+
+        # Historique
+        fig.add_trace(go.Scatter(
+            x=df_series.index,
+            y=df_series['y'],
+            mode='lines',
+            name='Historique'
+        ))
+
+        # Prévision
+        fig.add_trace(go.Scatter(
+            x=df_forecast.index,
+            y=df_forecast['y'],
+            mode='lines',
+            name='Prévision',
+            line=dict(dash='dash')
+        ))
+
+        fig.update_layout(
+            title='Prévision avec Darts',
+            xaxis_title='Date',
+            yaxis_title='Valeur',
+            template='plotly_white'
         )
-        return forecast_fig
+        return fig
