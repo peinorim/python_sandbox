7e9fd476f6d9a82766a19e564dcb911085ad2ab1
peinorim <peinorim@gmail.com>
2026-07-08 14:27:43 +0200

    updates

--- stock/finance.py
+++ stock/finance.py
@@ -71,12 +71,14 @@ class StockAPI:
         stocks = []
         for symbol in symbols:
             cache_key = f"{symbol}-{start_date}-{periods}"
-            if not redis_conn.get(cache_key):
+            cached = redis_conn.get(cache_key)
+
+            if not cached:
                 info, data = self.get_stock_data(symbol=symbol, start_date=start_date)
                 figure = Forecast().render_figure(symbol=symbol, info=info, data=data, periods=periods)
                 redis_conn.set(cache_key, pickle.dumps(figure), ex=EXPIRE_CACHE_SECONDS)
             else:
-                figure = pickle.loads(redis_conn.get(cache_key))
+                figure = pickle.loads(cached)
 
             if to_html:
                 figure.write_html(f"offline/{symbol}.html", include_plotlyjs='cdn', full_html=False)
